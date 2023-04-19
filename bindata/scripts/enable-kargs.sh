c80a7e3434225df8a390708885b69bf4d135a06f
Oz N Tiram <oz@spectrocloud.com>
2023-04-19 14:33:27 +0200

    enable-kargs.sh - add support for debian OS family
    Grubby, which is found in RHEL based OS family isn't packaged
    for Debian OS family.
    This patch enables the same functionality using sed.
    
    Signed-off-by: Oz N Tiram <oz@spectrocloud.com>

--- bindata/scripts/enable-kargs.sh
+++ bindata/scripts/enable-kargs.sh
@@ -4,8 +4,12 @@ set -x
 declare -a kargs=( "$@" )
 ret=0
 args=$(chroot /host/ cat /proc/cmdline)
+os_family=$(chroot /host/ grep ID_LIKE /etc/os-release | cut -d '=' -f2)
 
-if chroot /host/ test -f /run/ostree-booted ; then
+if [ "${os_family}" == "debian" ]; then
+    sed -i '/^GRUB_CMDLINE_LINUX=/s/"$/ '"$*"'"/g' /etc/default/grub
+    let ret++
+elif chroot /host/ test -f /run/ostree-booted ; then
     for t in "${kargs[@]}";do
         if [[ $args != *${t}* ]];then
             if chroot /host/ rpm-ostree kargs | grep -vq ${t}; then
